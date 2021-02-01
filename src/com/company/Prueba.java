c6fe0804cbf990208f08e6fda0c22625a8374002
aitorsp-elpuig <aitorsp@elpuig.xeill.net>
2021-02-01 13:37:15 +0100

    Update Javadoc

---
+++ src/com/company/Prueba.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Clase para probar Javadoc
+ * @author Aitor
+ */
+public class Prueba {
+
+    /**
+     * Metodo para sumar
+     * @param a primer numero
+     * @param b segundo numero
+     * @return retorna la suma de a+b
+     */
+    public int Numeros (int a, int b){
+        return a+b;
+    }
+}
